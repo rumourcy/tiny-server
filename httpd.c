8098d9dddd1e7f785bd3f5af58a95808c996b3cb
trierbo <trier@keessi.com>
2017-07-27 17:17:51 +0800

    get_line

--- httpd.c
+++ httpd.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 void error_die(const char*);
 int startup(u_short*);
+int get_line(int sock, char* buf, int size);
 
 /**
  * 异常退出函数
@@ -63,6 +65,37 @@ int startup(u_short* port) {
   return httpd;
 }
 
+/**
+ * 使用连接socket接受数据
+ * 每次读取一行数据
+ * 每行数据以回车或者\r\n结束
+ */
+int get_line(int sock, char* buf, int size) {
+  int i = 0;
+  char c = '\0';
+  int n;
+  // 终止符统一设置为\n
+  while((i < size - 1) && (c != '\n')) {
+    n = recv(sock, &c, 1, 0);
+    if(n > 0) {
+      if(c == '\r') {
+        // MSG_PEEK 查看下个字符，但是位置没变
+        n = recv(sock, &c, 1, MSG_PEEK);
+        if((n > 0) && (c == '\n'))
+          // 获取之前查看的字符
+          recv(sock, &c, 1, 0);
+        else
+          c = '\n';
+      }
+      buf[i] = c;
+      i++;
+    } else
+      c = '\n';
+  }
+  buf[i] = '\0';
+  return i;
+}
+
 int main() {
   // 监听socket
   int server_sock = -1;
@@ -78,6 +111,11 @@ int main() {
     if(client_sock == -1)
       error_die("创建连接socket失败");
     printf("%s\n", "success");
+    char buf[1024];
+    get_line(client_sock, buf, sizeof(buf));
+    printf("%s", buf);
+    close(client_sock);
   }
+  close(server_sock);
   return 0;
 }
